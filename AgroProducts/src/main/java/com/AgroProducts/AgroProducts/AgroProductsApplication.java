712d53e7b456ec680da81649ec2bc4feae5b4b3f
ramak1247 <88154229+ramak1247@users.noreply.github.com>
2022-03-17 23:29:48 -0700

    First Commit

---
+++ AgroProducts/src/main/java/com/AgroProducts/AgroProducts/AgroProductsApplication.java
@@ -0,0 +1,13 @@
+package com.AgroProducts.AgroProducts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgroProductsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgroProductsApplication.class, args);
+	}
+
+}
---
+++ AgroProducts/src/test/java/com/AgroProducts/AgroProducts/AgroProductsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.AgroProducts.AgroProducts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgroProductsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
